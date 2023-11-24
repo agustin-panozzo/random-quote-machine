c64a50b36524428830bda284bdd906cfd4835819
agustin-panozzo <apanozzo@fi.uba.ar>
2023-11-24 18:11:04 -0300

    agregado iconos

--- src/quoteBlock.js
+++ src/quoteBlock.js
@@ -4,6 +4,8 @@ import { changePhrase } from './redux/slice';
 import { Howl } from 'howler';
 import sound from './sounds/mario.mp3';
 import './styles.scss';
+import { FaInstagram, FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
+import { BsTwitterX } from "react-icons/bs";
 
 const QuoteBlock = () => {
     const text = useSelector(state => state.phrase.text);
@@ -28,12 +30,13 @@ const QuoteBlock = () => {
     return (
         <div>
             <div id='quote-box'>
-                <h1 className={`phrase`} key={animationKey} id='text'><strong>{text}</strong></h1>
-                <p id='author'>{author}</p>
+                <h1 className={`phrase`} key={animationKey} id='text'><strong><FaQuoteLeft size={30} /> {text} <FaQuoteRight size={30} /></strong></h1>
+                <p id='author'>- <i>{author}</i></p>
                 <button id='new-quote' onClick={() => dispatch(changePhrase())}>
-                    Generar nueva frase
+                    Nueva Frase
                 </button>
-                <a id='tweet-quote' href='#author'>Link</a>
+                <a id='tweet-quote' href='https://x.com' target='_blank' rel="noopener noreferrer"><BsTwitterX size={32}/></a>
+                <a id="instagram-quote" href='https://instagram.com' target='_blank' rel="noopener noreferrer"><FaInstagram size={32}/></a>
             </div>
         </div>
     )
